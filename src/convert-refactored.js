d7ca8b0549795c328a7f5bfcc518c30e83b3b73c
name <email>
2023-06-26 04:07:34 +0500

    third commit

--- src/convert-refactored.js
+++ src/convert-refactored.js
@@ -1,14 +1,30 @@
 const data = require('./data')
 const fs = require('fs')
 data.forEach(obj => {
-  obj["Number of Hashtags"] = Math.floor(Math.random() * (1000 - 500 + 1)) + 500;
-  obj["Number of Mentions"] = Math.floor(Math.random() * (300 - 100 + 1)) + 100;
-  obj["Number of Tweeted"] = Math.floor(Math.random() * (5000 - 1000 + 1)) + 1000;
+  obj["Number of Hashtags"] = Math.floor(Math.random() * (2000 - 10000 + 1)) + 10000;
+  obj["Number of Mentions"] = Math.floor(Math.random() * (2000 - 10000 + 1)) + 10000;
+  obj["Number of Tweeted"] = Math.floor(Math.random() * (20000 - 10000 + 1)) + 10000;
   obj["Number of Post Liked"] = Math.floor(Math.random() * (5000 - 1000 + 1)) + 1000;
-  obj["Number of User Liked"] = Math.floor(Math.random() * (300 - 100 + 1)) + 100;
+  obj["Number of User Liked"] = Math.floor(Math.random() * (5000 - 1000 + 1)) + 1000;
   obj["Positive Users"] = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
   obj["Negative Users"] = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
   obj["Neutral Users"] = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+  obj["Number of Retweeted"] = Math.floor(Math.random() * (5000 - 1000 + 1)) + 1000;
+  obj["Number of User Retweeted"] = Math.floor(Math.random() * (5000 - 1000 + 1)) + 1000;
+  obj["Sum of Positive Hashtag"] = Math.floor(Math.random() * (5000 - 1000 + 1)) + 1000;
+  obj["Sum of Positive Mentions"] = Math.floor(Math.random() * (5000 - 1000 + 1)) + 1000;
+  obj["Sum of Negative Hashtag"] = Math.floor(Math.random() * (5000 - 1000 + 1)) + 1000;
+  obj["Sum of Negative Mentions"] = Math.floor(Math.random() * (5000 - 1000 + 1)) + 1000;
+  obj["Sum of Query Hashtag"] = Math.floor(Math.random() * (5000 - 1000 + 1)) + 1000;
+  obj["Sum of Query Mentions"] = Math.floor(Math.random() * (5000 - 1000 + 1)) + 1000;
+  obj["Positive Hashtag Users"] = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+  obj["Negative Hashtag Users"] = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+  obj["Query Hashtag Users"] = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+  obj["Query Mention Users"] = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+  obj["Neutral Hashtag Users"] = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+  obj["Positive Mentions Users"] = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+  obj["Negative Mentions Users"] = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+  obj["Neutral Mentions Users"] = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
 })
 
 const jsonContent = `const jsonData = ${JSON.stringify(data, null, 2)};\n\nmodule.exports = jsonData;`;
--- src/queries.js
+++ src/queries.js
@@ -1,4 +1,4 @@
-const data = require('./data')
+const data = require('./data-refactored.js')
 
 export const getAccountData = (accountName, year) => {
   const accountData = data.filter(record => (record.Accounts === accountName && record['Years (Date)'] === year))
@@ -7,12 +7,23 @@ export const getAccountData = (accountName, year) => {
   const positive = []
   const tweets = []
   const queries = []
+  const hashtags = []
+  const mentions = []
+  const likedPosts = []
+  const likedUsers = []
+  const tweeted = []
 
-  accountData.map(rec => {
+
+  accountData.forEach(rec => {
     negative.push(rec['Sum of Negative'])
     positive.push(rec['Sum of Positive'])
     tweets.push(rec['Sum of Tweets'])
     queries.push(rec['Sum of Queries'])
+    hashtags.push(rec['Number of Hashtags'])
+    mentions.push(rec['Number of Mentions'])
+    likedPosts.push(rec['Number of Post Liked'])
+    likedUsers.push(rec['Number of User Liked'])
+    tweeted.push(rec['Number of Tweeted'])
   })
   
   series.push({
@@ -32,6 +43,288 @@ export const getAccountData = (accountName, year) => {
   series.push({
     name: "Sum of Negative",
     data: negative
+  })
+
+  series.push({
+    name: "Number of Hashtags",
+    data: hashtags
+  })
+
+  series.push({
+    name: "Number of Mentions",
+    data: mentions
+  })
+
+  series.push({
+    name: "Liked Posts",
+    data: likedPosts
+  })
+
+  series.push({
+    name: "Number of User Liked Posts",
+    data: likedUsers
+  })
+
+  series.push({
+    name: "Number of Hashtags",
+    data: tweeted
+  })
+
+  return series
+} 
+
+export const getTweetsData = (accountName, year) => {
+  const accountData = data.filter(record => (record.Accounts === accountName && record['Years (Date)'] === year))
+  const series = []
+  const tweets = []
+  const hashtags = []
+  const mentions = []
+  const tweeted = []
+
+
+  accountData.forEach(rec => {
+    tweets.push(rec['Sum of Tweets'])
+    hashtags.push(rec['Number of Hashtags'])
+    mentions.push(rec['Number of Mentions'])
+    tweeted.push(rec['Number of Tweeted'])
+  })
+  
+  series.push({
+    name: "Sum of Tweets",
+    data: tweets
+  })
+
+  series.push({
+    name: "Number of Hashtags",
+    data: hashtags
+  })
+
+  series.push({
+    name: "Number of Mentions",
+    data: mentions
+  })
+
+
+  series.push({
+    name: "Number of Tweeted",
+    data: tweeted
+  })
+
+  return series
+} 
+
+export const getLikesData = (accountName, year) => {
+  const accountData = data.filter(record => (record.Accounts === accountName && record['Years (Date)'] === year))
+  const series = []
+
+  const likedPosts = []
+  const likedUsers = []
+
+
+  accountData.forEach(rec => {
+    likedPosts.push(rec['Number of Post Liked'])
+    likedUsers.push(rec['Number of User Liked'])
+  })
+
+  series.push({
+    name: "Liked Posts",
+    data: likedPosts
+  })
+
+  series.push({
+    name: "Number of User Liked Posts",
+    data: likedUsers
+  })
+
+  return series
+} 
+
+export const getReTweetsData = (accountName, year) => {
+  const accountData = data.filter(record => (record.Accounts === accountName && record['Years (Date)'] === year))
+  const series = []
+
+  const retweetedTweets = []
+  const retweetedUsers = []
+
+
+  accountData.forEach(rec => {
+    retweetedTweets.push(rec['Number of Retweeted'])
+    retweetedUsers.push(rec['Number of User Retweeted'])
+  })
+
+  series.push({
+    name: "Retweeted tweets",
+    data: retweetedTweets
+  })
+
+  series.push({
+    name: "Number of User Retweeted",
+    data: retweetedUsers
+  })
+
+  return series
+} 
+
+export const getTweetCategoryData = (accountName, year) => {
+  const accountData = data.filter(record => (record.Accounts === accountName && record['Years (Date)'] === year))
+  const series = []
+
+  const positiveTweets = []
+  const negativeTweets = []
+  const queryTweets = []
+
+
+  accountData.forEach(rec => {
+    positiveTweets.push(rec['Sum of Positive'])
+    negativeTweets.push(rec['Sum of Negative'])
+    queryTweets.push(rec['Sum of Queries'])
+  })
+
+  series.push({
+    name: "Positive tweets",
+    data: positiveTweets
+  })
+
+  series.push({
+    name: "Negative tweets",
+    data: negativeTweets
+  })
+
+  series.push({
+    name: "Neutral/Query tweets",
+    data: queryTweets
+  })
+
+  return series
+} 
+
+export const getHashtagTweetCategoryData = (accountName, year) => {
+  const accountData = data.filter(record => (record.Accounts === accountName && record['Years (Date)'] === year))
+  const series = []
+
+  const positiveTweets = []
+  const negativeTweets = []
+  const queryTweets = []
+
+
+  accountData.forEach(rec => {
+    positiveTweets.push(rec['Sum of Positive Hashtag'])
+    negativeTweets.push(rec['Sum of Negative Hashtag'])
+    queryTweets.push(rec['Sum of Query Hashtag'])
+  })
+
+  series.push({
+    name: "Positive tweets",
+    data: positiveTweets
+  })
+
+  series.push({
+    name: "Negative tweets",
+    data: negativeTweets
+  })
+
+  series.push({
+    name: "Neutral/Query tweets",
+    data: queryTweets
+  })
+
+  return series
+} 
+
+export const getMentionTweetCategoryData = (accountName, year) => {
+  const accountData = data.filter(record => (record.Accounts === accountName && record['Years (Date)'] === year))
+  const series = []
+
+  const positiveTweets = []
+  const negativeTweets = []
+  const queryTweets = []
+
+
+  accountData.forEach(rec => {
+    positiveTweets.push(rec['Sum of Positive Mentions'])
+    negativeTweets.push(rec['Sum of Negative Mentions'])
+    queryTweets.push(rec['Sum of Query Mentions'])
+  })
+
+  series.push({
+    name: "Positive tweets",
+    data: positiveTweets
+  })
+
+  series.push({
+    name: "Negative tweets",
+    data: negativeTweets
+  })
+
+  series.push({
+    name: "Neutral/Query tweets",
+    data: queryTweets
+  })
+
+  return series
+} 
+
+export const getPositiveMentionUserData = (accountName, year) => {
+  const accountData = data.filter(record => (record.Accounts === accountName && record['Years (Date)'] === year))
+  const series = []
+
+  const positiveTweets = []
+  const negativeTweets = []
+  const queryTweets = []
+
+
+  accountData.forEach(rec => {
+    positiveTweets.push(rec['Positive Mentions Users'])
+    negativeTweets.push(rec['Negative Mentions Users'])
+    queryTweets.push(rec['Query Mention Users'])
+  })
+
+  series.push({
+    name: "Positive users",
+    data: positiveTweets
+  })
+
+  series.push({
+    name: "Negative users",
+    data: negativeTweets
+  })
+
+  series.push({
+    name: "Neutral/Query users",
+    data: queryTweets
+  })
+
+  return series
+} 
+
+export const getPositiveHashtagUserData = (accountName, year) => {
+  const accountData = data.filter(record => (record.Accounts === accountName && record['Years (Date)'] === year))
+  const series = []
+
+  const positiveTweets = []
+  const negativeTweets = []
+  const queryTweets = []
+
+
+  accountData.forEach(rec => {
+    positiveTweets.push(rec['Positive Hashtag Users'])
+    negativeTweets.push(rec['Negative Hashtag Users'])
+    queryTweets.push(rec['Query Hashtag Users'])
+  })
+
+  series.push({
+    name: "Positive users",
+    data: positiveTweets
+  })
+
+  series.push({
+    name: "Negative users",
+    data: negativeTweets
+  })
+
+  series.push({
+    name: "Neutral/Query users",
+    data: queryTweets
   })
 
   return series
